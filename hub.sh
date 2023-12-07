3ce72ac1c370689109bc06604eec68a0ed900e2b
Bin01Sys <zar0.theuz@gmail.com>
2023-12-07 10:29:40 -0300

    Ajustando...

--- hub.sh
+++ hub.sh
@@ -1,54 +1,49 @@
 #!/usr/bin/bash
 
-set -e # Para o script em caso de erro
+# Parar o script em caso de erro
+set -e
 
 check_tools() {
-        tool_name=$1
+    tool_name=$1
 
-        # Verifica se a ferramenta esta disponivel no sistema
-        if command -v "$tool_name" &> /dev/null; then
-                echo "[ ✓ ] $tool_name está instalado"
-                echo ""
+    # Verifica se a ferramenta está disponível no sistema
+    if command -v "$tool_name" &> /dev/null; then
+        echo "[ ✓ ] $tool_name está instalado."
+    else
+        echo "[ * ] $tool_name não está instalado. Tentando instalar..."
+
+        if [ "$tool_name" == "termux-battery-status" ]; then
+            pkg install termux-api && echo "[ ✓ ] Instalação concluída com sucesso."
+        elif [ "$tool_name" == "jq" ]; then
+            pkg install jq && echo "[ ✓ ] Instalação concluída com sucesso."
         else
-                echo "[ * ] $tool_name não está instalado. Tentando instalar..."
-                echo ""
-
-                if [ "$tool_name" == "termux-battery-status" ]; then
-                        pkg install termux-api && echo "[ ✓ ] Instalação concluída com sucesso."
-                        echo ""
-                elif [ "$tool_name" == "jq" ]; then
-                        pkg install jq && echo "[ ✓ ] Instalação concluída com sucesso."
-                        echo ""
-                else
-                        echo "[ x ] Ferramenta desconhecida. Não é possível instalar automaticamente."
-                        echo ""
-                fi
+            echo "[ x ] Ferramenta desconhecida. Não é possível instalar automaticamente."
         fi
-	
-	#Esperar uns 10s para limpar a tela
-	sleep 10
+
+        # Adiciona uma pausa de 2 segundos antes de limpar a tela
+        sleep 2
         clear
+    fi
 }
 
 get_battery_status() {
-        result=$(termux-battery-status)
-        charge_level=$(echo "$result" | jq -r '.percentage')
+    # Obter o status da bateria
+    result=$(termux-battery-status)
+    charge_level=$(echo "$result" | jq -r '.percentage')
 
-        echo "Nível de carga da tua bateria: $charge_level%"
-        echo ""
+    echo "Nível de carga da tua bateria: $charge_level%"
 }
 
 get_network_info() {
-        result=$(termux-network-status)
-        connect_status=$(echo "$result" | jq -r '.isConnected')
+    # Obter informações da rede
+    result=$(termux-network-status)
+    connect_status=$(echo "$result" | jq -r '.isConnected')
 
-        if [ "$connect_status" == true ]; then
-                echo "O dispositivo está conectado à internet."
-                echo ""
-        else
-                echo "O dispositivo não está conectado à internet."
-                echo ""
-        fi
+    if [ "$connect_status" == true ]; then
+        echo "O dispositivo está conectado à internet."
+    else
+        echo "O dispositivo não está conectado à internet."
+    fi
 }
 
 # Chamando as funções
@@ -56,3 +51,4 @@ check_tools termux-api
 check_tools jq
 get_battery_status
 get_network_info
+
